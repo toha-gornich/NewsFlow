b92bd9f15045a8147d1a209e4d28ddf8907e0650
toha-gornich <92648253+toha-gornich@users.noreply.github.com>
2026-03-04 17:59:38 +0200

    Init project

---
+++ NewsFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFlow
+//
+//  Created by Горніч Антон on 04.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsFlowTests/NewsFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFlowTests.swift
+//  NewsFlowTests
+//
+//  Created by Горніч Антон on 04.03.2026.
+//
+
+import Testing
+@testable import NewsFlow
+
+struct NewsFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
